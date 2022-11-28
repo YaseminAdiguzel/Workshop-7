75536c365268286f0869b954ba77faf01c321e0a
YaseminAdiguzel <113547196+YaseminAdiguzel@users.noreply.github.com>
2022-11-28 16:10:30 +0300

    Add files via upload

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Products } from '../models/products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private httpClient:HttpClient) {  }
+  getProductsList(): Observable<Products[]>{
+    return this.httpClient.get<Products[]>("http://localhost:3000/products");
+  }
+}
